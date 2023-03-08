8a5a9522ae6b228d855742bf2d4b12ff10d14a73
Aaron Rohrbacher <arjazz@icloud.com>
2023-03-07 16:08:33 -0800

    adds eslint, ran a lint

--- userData.js
+++ userData.js
@@ -1,10 +1,9 @@
 const serverless = require('serverless-http');
-const express = require('express'),
-  CognitoExpress = require('cognito-express')
-const app = express()
+const express = require('express');
+const CognitoExpress = require('cognito-express');
+const app = express();
 const AWS = require('aws-sdk');
 
-const USERS_TABLE = process.env.USERS_TABLE;
 
 const IS_OFFLINE = process.env.IS_OFFLINE;
 
@@ -13,50 +12,49 @@ let cognitoExpress;
 if (IS_OFFLINE === 'true') {
   dynamoDb = new AWS.DynamoDB.DocumentClient({
     region: 'localhost',
-    endpoint: 'http://localhost:8000'
-  })
+    endpoint: 'http://localhost:8000',
+  });
   console.log(dynamoDb);
 } else {
   dynamoDb = new AWS.DynamoDB.DocumentClient();
   cognitoExpress = new CognitoExpress({
-    region: "us-west-2",
+    region: 'us-west-2',
     cognitoUserPoolId: process.env.user_pool_id,
-    tokenUse: "id", //Possible Values: access | id
-    tokenExpiration: 3600000 //Up to default expiration of 1 hour (3600000 ms)
+    tokenUse: 'id', // Possible Values: access | id
+    tokenExpiration: 3600000, // Up to default expiration of 1 hour (3600000 ms)
   });
-
 };
 
 
-app.post('/', function (req, res) {
+app.post('/', function(req, res) {
   const params = {
     TableName: process.env.BUDGETS_TABLE,
     Key: {
       budgetId: req.params.budgetId,
     },
-  }
+  };
 
   dynamoDb.get(params, (error, result) => {
     if (error) {
       console.log(error);
-      res.status(400).json({ error: 'Could not get budgets' });
+      res.status(400).json({error: 'Could not get budgets'});
     }
     if (result.Item) {
-      const { budgetName } = result.Item;
-      res.json({ userId, budgetName });
+      const {budgetName} = result.Item;
+      res.json({userId, budgetName});
     } else {
-      res.status(404).json({ error: "budgets not found" });
+      res.status(404).json({error: 'budgets not found'});
     }
   });
-})
+});
 
-app.post('/budgets', function (req, res) {
-  const { userId, budgetName } = JSON.parse(req.apiGateway.event.body);
+app.post('/budgets', function(req, res) {
+  const {userId, budgetName} = JSON.parse(req.apiGateway.event.body);
   if (typeof userId !== 'string') {
-    console.log(userId)
-    return res.status(400).json({ error: '"userId" must be a string' });
+    console.log(userId);
+    return res.status(400).json({error: '"userId" must be a string'});
   } else if (typeof budgetName !== 'string') {
-    return res.status(400).json({ error: '"name" must be a string' });
+    return res.status(400).json({error: '"name" must be a string'});
   }
 
   const params = {
@@ -72,10 +70,9 @@ app.post('/budgets', function (req, res) {
       console.log(error);
       return res.status(400).json( budgetName );
     }
-    res.json({ userId, budgetName });
+    res.json({userId, budgetName});
   });
 });
-
 
 
 module.exports.handler = serverless(app);
