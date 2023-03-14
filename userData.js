ce84a5abf38371ef58a87d30cec06287b5e7cc78
Aaron Rohrbacher <arjazz@icloud.com>
2023-03-14 16:08:19 -0700

    take one did not work

--- userData.js
+++ userData.js
@@ -1,12 +1,30 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const CognitoExpress = require('cognito-express');
+const AWS = require('aws-sdk');
 const IS_OFFLINE = process.env.IS_OFFLINE;
-import awsInit from './awsInitializers/cognitoDynamo'
 
-const {dynamoDb, cognitoExpress} = awsInit();
+let dynamoDb;
+let cognitoExpress;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+  });
+  console.log(dynamoDb);
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+  cognitoExpress = new CognitoExpress({
+    region: 'us-west-2',
+    cognitoUserPoolId: process.env.user_pool_id,
+    tokenUse: 'id', // Possible Values: access | id
+    tokenExpiration: 3600000, // Up to default expiration of 1 hour (3600000 ms)
+  });
+}
 
 app.post('/userBudgets', function(req, res) {
+  console.log(dynamoDb);
   const {userId} = JSON.parse(req.apiGateway.event.body);
   const params = {
     TableName: process.env.BUDGETS_TABLE,
