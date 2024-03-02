61353e739064a1b3e5e16ac98e321383f059b6d7
Aaron Rohrbacher <arjazz@icloud.com>
2024-03-02 03:17:46 -0800

    introduce unit testing yes very late

--- __tests__/userData.test.js
+++ __tests__/userData.test.js
@@ -5,5 +5,5 @@ test('determinePayPeriod properly determines the correct pay period', () => {
     dateSpy = jest.spyOn(global.Date, 'now').mockImplementation(() => Date.parse('03-01-2024'));
     expect(functions.determinePayPeriod()).toBe(1);
     dateSpy = jest.spyOn(global.Date, 'now').mockImplementation(() => Date.parse('03-15-2024'));
-    expect(functions.determinePayPeriod()).toBe(1);
+    expect(functions.determinePayPeriod()).toBe(15);
 });
